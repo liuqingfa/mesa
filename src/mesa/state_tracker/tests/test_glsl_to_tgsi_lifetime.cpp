8e3c439cb14645cc00f327b9de23a0a6aea742b3
Gert Wollny <gw.fossdev@gmail.com>
2018-03-03 10:51:52 +0100

    mesa/st/glsl_to_tgsi: Properly resolve life times simple if/else + use constructs
    in constructs like below, currently the live range estimation extends the live range
    of t unecessarily to the whole loop because it was not detected that t is
    unconditional written and later read only in the "if (a)" scope.
    
      while (foo)  {
        ...
        if (a) {
           ...
           if (b)
             t = ...
           else
             t = ...
           x = t;
           ...
        }
         ...
      }
    
    This patch adds a unit test for this case and corrects the minimal live range estimation
    accordingly.

--- src/mesa/state_tracker/tests/test_glsl_to_tgsi_lifetime.cpp
+++ src/mesa/state_tracker/tests/test_glsl_to_tgsi_lifetime.cpp
@@ -794,6 +794,29 @@ TEST_F(LifetimeEvaluatorExactTest, WriteInIfElseBranchSecondIfInLoop)
    run (code, temp_lt_expect({{-1,-1}, {2,9}}));
 }
 
+/*
+  Test for
+*/
+
+TEST_F(LifetimeEvaluatorExactTest, DeeplyNestedinLoop)
+{
+   const vector<FakeCodeline> code = {
+      { TGSI_OPCODE_BGNLOOP },
+      {   TGSI_OPCODE_UIF, {}, {in0}, {}},
+      {     TGSI_OPCODE_FSEQ, {1}, {in1,in2}, {}},
+      {     TGSI_OPCODE_UIF, {}, {1}, {}},
+      {       TGSI_OPCODE_MOV, {2}, {in1}, {}},
+      {     TGSI_OPCODE_ELSE },
+      {       TGSI_OPCODE_MOV, {2}, {in2}, {}},
+      {     TGSI_OPCODE_ENDIF },
+      {     TGSI_OPCODE_MOV, {3}, {2}, {}},
+      {   TGSI_OPCODE_ENDIF },
+      {   TGSI_OPCODE_ADD, {out0}, {3, in1}, {}},
+      { TGSI_OPCODE_ENDLOOP }
+   };
+   run (code, temp_lt_expect({{-1,-1}, {2,3}, {4, 8}, {0,11}}));
+}
+
 /** Regression test for bug #104803,
  *  Read and write in if/else path outside loop and later read in conditional
  *  within a loop. The first write is to be considered the dominant write.
